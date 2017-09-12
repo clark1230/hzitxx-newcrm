8c735a481a219e7a693d1f124656b1d88280ec74
Imeiyang <15707974972@163.com>
2017-09-12 15:06:35 +0800

    nono

--- src/main/webapp/script/import/importPage.js
+++ src/main/webapp/script/import/importPage.js
@@ -70,29 +70,4 @@ $(function(){
 
         }
     });
-
-    //$("#import").click(function(){
-
-        //var formData = new FormData($( "#importForm" )[0]);
-        //$.ajax({
-        //    url: '/import' ,
-        //    type: 'POST',
-        //    data: formData,
-        //    async: false,
-        //    cache: false,
-        //    contentType: false,
-        //    processData: false,
-        //    success: function (resp) {
-        //        console.log(resp);
-        //    },
-        //    error: function (resp) {
-        //        console.log(resp);
-        //    }
-        //});
-
-        //$.post("/import",$("#importFrom").serialize(),function(resp){
-        //    console.log(resp);
-        //},"JSON");
-    //});
-
 });
