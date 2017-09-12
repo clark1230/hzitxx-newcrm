e3de775c67430790cab830eaf57a7f276214a151
lvyou12 <983566073@qq.com>
2017-09-12 10:31:47 +0800

    修复getServletContext报空指针

--- src/main/java/com/hzitshop/web/controllers/ImportInfoController.java
+++ src/main/java/com/hzitshop/web/controllers/ImportInfoController.java
@@ -59,6 +59,8 @@ public class ImportInfoController {
 //            licenseName字段为分校的校区名
 //            由于登入功能暂时没实现，获取不到session中的数据，暂时固定写死
 //            String licenseName = "宝安";
+
+
             String path = request.getSession().getServletContext().getRealPath("/");
             File f = new File(path+"/excel/"+file.getOriginalFilename());
             if(!f.exists()){
